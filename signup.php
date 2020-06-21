4a2490b04cc66f6a56b12b1cdc40d99ba86cfadf
Raihan Ahmed <raihan.tusher@yahoo.com>
2020-06-21 19:26:05 +0600

    sign up card

--- signup.php
+++ signup.php
@@ -29,40 +29,41 @@ if(isset($_POST["submit"])){
 }
 ifLoggedIn();
 ?>
-<div class="container">
-	<div class="row">
+<div class="container mt-5">
+	<div class="row justify-content-center">
 		<div class="col-7">
-			<form method="POST">
+			<div class="card">
+				<div class="card-header">Sign up</div>
+				<div class="card-body">
+					<form method="POST">
+						<div class="form-group">
+						<label >UserName:</label>
+						<input type="text" class="form-control" name="username">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+
+						<div class="form-group">
+						<label >Full Name:</label>
+						<input type="text" class="form-control" name="fullname">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+
+						<div class="form-group">
+						<label >Email:</label>
+						<input type="text" class="form-control" name="email">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+
+						<div class="form-group">
+						<label >Password:</label>
+						<input type="password" class="form-control" name="password">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+						</div>
+						<button type="submit" name="submit" value="submit" class="btn btn-primary">Sign Up</button>
+					</form>
+				</div>
+			</div>
 		
-				  <div class="form-group">
-				    <label >UserName:</label>
-				    <input type="text" class="form-control" name="username">
-				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				  </div>
-
-				  <div class="form-group">
-				    <label >Full Name:</label>
-				    <input type="text" class="form-control" name="fullname">
-				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				  </div>
-
-				  <div class="form-group">
-				    <label >Email:</label>
-				    <input type="text" class="form-control" name="email">
-				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				  </div>
-
-				  <div class="form-group">
-				    <label >Password:</label>
-				    <input type="password" class="form-control" name="password">
-				    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				  </div>
-			
-			
-
-				
-				  <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-				</form>
 		</div>
 	</div>
 </div>
